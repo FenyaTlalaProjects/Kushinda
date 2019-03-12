1789c60e7ddc7142521c32634e8b1317abf8ca15
Mohapi27 <cassino.happies@gmail.com>
2019-03-12 10:33:38 +0200

    Kusinda commit

---
+++ src/main/java/za/co/fenya/tlala/fenyatlala/FenyatlalaApplication.java
@@ -0,0 +1,14 @@
+package za.co.fenya.tlala.fenyatlala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenyatlalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FenyatlalaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/za/co/fenya/tlala/fenyatlala/FenyatlalaApplicationTests.java
@@ -0,0 +1,17 @@
+package za.co.fenya.tlala.fenyatlala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FenyatlalaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
